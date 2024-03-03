a2d795b6aedbe6d1eff2548c2c81661c4cc81c6c
Christian Url <christianurl@MacBook-Pro-von-Christian.local>
2024-03-03 22:21:33 +0100

    bug fixing

--- comext/3_plots-strucchange.R
+++ comext/3_plots-strucchange.R
@@ -73,26 +73,41 @@ grid1 = df[TRADE_TYPE == "E",
 
 # flow: 1 = Import, 2 = Export
 
-plot_breaks <- function(df, cn_code, country, flow, metric){
+plot_breaks <- function(df, cn_code, country, measure, flow, metric, transformation = "log", confInt){
+  
+  stopifnot(measure %in% c("quantity", "value"), flow %in% 1:2, metric %in% c("BIC", "RSS"),
+            transformation %in% c("none", "log"))
   
   df1= df[FLOW == flow & PARTNER_ISO == country & CN6 == cn_code, 
-     sum(VALUE_IN_EUROS, na.rm = T), 
+     .(VALUE_SUM = sum(VALUE_IN_EUROS, na.rm = T), QUANTITY_SUM = sum(QUANTITY_IN_KG, na.rm = T)), 
      by = c("PERIOD", "PARTNER_ISO")]
-  colnames(df1)[2:3] <- c("DESTINATION","VALUE_SUM")
+  colnames(df1)[2] <- c("DESTINATION")
   df1[, PERIOD := lubridate::ym(PERIOD)]
   
   years = 2018:2023
   months = 1:12  %>% as.character() %>% str_c("0",.) %>% str_sub(-2)
+  
+  naFillValue = switch(transformation,
+                       "none" = 0,
+                       "log" = 1)
   
   tsgrid =  expand_grid(years, months) %>%
     mutate(PERIOD = ym(str_c(years, months)),
            DESTINATION = country) %>%
     select(PERIOD, DESTINATION) %>%
     left_join(df1, by = join_by("PERIOD", "DESTINATION")) %>%
-    mutate(VALUE_SUM = replace_na(VALUE_SUM, 1))
+    mutate(QUANTITY_SUM = pmax(QUANTITY_SUM, naFillValue, na.rm = T),
+           VALUE_SUM = pmax(VALUE_SUM, naFillValue, na.rm = T))
   rm(df1)
+  tscol = switch(measure,
+                 "quantity" = tsgrid$QUANTITY_SUM,
+                 "value" = tsgrid$VALUE_SUM)
   
-  ts1 = ts(log(tsgrid$VALUE_SUM), start = 2018, end = c(2023,12), frequency = 12)
+  tscolTrans = switch(transformation,
+                      "none" = tscol,
+                      "log" = log(tscol))
+  
+  ts1 = ts(tscolTrans, start = 2018, end = c(2023,12), frequency = 12)
   bp.tr = breakpoints(ts1 ~ 1)
   
   a = which(summary(bp.tr)$RSS["RSS",]==min(summary(bp.tr)$RSS["RSS",]))
@@ -101,6 +116,7 @@ plot_breaks <- function(df, cn_code, country, flow, metric){
   breaksNum = switch(metric,
                      "RSS" = as.numeric(names(a)),
                      "BIC" = as.numeric(names(b)))
+  
   if(length(breaksNum) > 1) breaksNum = breaksNum[1]
   if(breaksNum==0){
     stop("No breaks estimated")
@@ -109,19 +125,22 @@ plot_breaks <- function(df, cn_code, country, flow, metric){
   fm0 <- lm(ts1 ~ 1)
   fm1 <- lm(ts1 ~ breakfactor(bp.tr, breaks = breaksNum))
   ci.bp = confint(bp.tr, breaks = breaksNum)
+  n = nrow(ci.bp$confint)
+  narow = unique(which(is.na(ci.bp$confint))%%n)
+  
   plot(ts1)
+  title(paste0("Country: ", country, ", CN Code: ", cn_code))
   lines(ts(fitted(fm0), start = 2018, end = c(2023,12), frequency = 12), col = 3)
   lines(ts(fitted(fm1),  start = 2018, end = c(2023,12), frequency = 12), col = 4)
-  lines(bp.tr)
-  lines(ci.bp)
-  title(main = paste0("Country: ", country, ", CN Code: ", cn_code))
+  if(!sum(narow) == 0 | !confInt) lines(bp.tr, breaks = breaksNum)
+  if(sum(narow) == 0 & confInt) lines(ci.bp)
 }
 
-plot_breaks(df, "854110", "RU", 2, "BIC")
+plot_breaks(df, "854110", "TR", "value",  2, "BIC", transformation = "log", confInt = T)
 
 # to do: 
-## transformation selector
 ## seasonality estimation
 ## implement estimation function
 ## time selector
 ## na handling (e.g. ARIMA fill, or others)
+## changepoint package
